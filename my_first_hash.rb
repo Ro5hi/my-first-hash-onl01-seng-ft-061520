da65941a6f80101607aa2195017842b49e08e3c5
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 17:12:05 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -42,9 +42,9 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   # add 4 gun powder to the shipping_manifest hash below
-  
-shipping_manifest = ["muskets" => 2, "gun powder" => 4]
-shipping_manifest >>
+
+
+shipping_manifest << ["muskets" => 2, "gun powder" => 4]
 
   
   # return the shipping_manifest hash below
