d6d2b3515844487158e67df5048c392e5549a4cd
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-21 17:09:11 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -43,12 +43,12 @@ def adding
   # add 2 muskets to the shipping_manifest hash below
   # add 4 gun powder to the shipping_manifest hash below
   
-H = Hash["muskets" => 2, "gun powder" => 4]
+shipping_manifest = ["muskets" => 2, "gun powder" => 4]
 
 puts "#{H['muskets']}"
 puts "#{H['gun powder']}"
   
   # return the shipping_manifest hash below
 
-  end
+
 end
